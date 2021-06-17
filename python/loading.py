d55054fe58eb464db1a6314736cc565b3a8fa241
KateLocker <kate.locker.wang@gmail.com>
2021-06-17 22:11:04 +0900

    insert and read data ok

--- python/loading.py
+++ python/loading.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("sample_data.csv")
 
-print(df.info())
+#print(df.info())
+
+dic = []
+
+for row in df.itertuples():
+    dic.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
+
+print(dic)
+
